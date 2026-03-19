35dd0346b469f3770d9d0bb1444990139bd85ed4
caxtonacollins <caxtonacollins@gmail.com>
2026-03-19 14:54:47 +0100

    fix: remove unused Column import from user-badge.entity.ts

--- libs/database/src/entities/user-badge.entity.ts
+++ libs/database/src/entities/user-badge.entity.ts
@@ -1,23 +1,29 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, JoinColumn, PrimaryColumn } from 'typeorm';
-import { User } from './user.entity';
-import { Badge } from './badge.entity';
+import {
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  JoinColumn,
+  PrimaryColumn,
+} from "typeorm";
+import { User } from "./user.entity";
+import { Badge } from "./badge.entity";
 
-@Entity('user_badges')
+@Entity("user_badges")
 export class UserBadge {
-  @PrimaryColumn({ name: 'user_id' })
+  @PrimaryColumn({ name: "user_id" })
   userId: string;
 
-  @PrimaryColumn({ name: 'badge_id' })
+  @PrimaryColumn({ name: "badge_id" })
   badgeId: number;
 
-  @CreateDateColumn({ name: 'earned_at' })
+  @CreateDateColumn({ name: "earned_at" })
   earnedAt: Date;
 
   @ManyToOne(() => User, (u) => u.badges)
-  @JoinColumn({ name: 'user_id' })
+  @JoinColumn({ name: "user_id" })
   user: User;
 
   @ManyToOne(() => Badge, (b) => b.earnedBy)
-  @JoinColumn({ name: 'badge_id' })
+  @JoinColumn({ name: "badge_id" })
   badge: Badge;
 }
